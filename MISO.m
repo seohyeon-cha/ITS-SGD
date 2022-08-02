28e42cba7624ce92f97f7aa54dc0f86c44a3cbb1
Seohyeon Cha <79090903+seohyeon-cha@users.noreply.github.com>
2022-08-02 22:37:22 +0900

    Update MISO.m

--- MISO.m
+++ MISO.m
@@ -1,12 +1,12 @@
 clear;
-load('channel_MISO.mat','phi_L','theta_L','path_gain','IRS_phase_vector','iteration','N_IRS','N_BS','L','lambda','d');
+load('channel_MISO.mat','phi_L','theta_L','path_gain','IRS_phase_vector','iteration','N_y','N_z','N_BS','L','lambda','d');
 
 %% SNR 따라서, rate 구하는 거 (iteration 100번) 
 tx_power = linspace(-10, 30, 10); %dBm
 rate_w = zeros(1,length(tx_power));
 % active antenna 일때는 d 바꿔야 하는 거 아닌가? (해보니까 결과는 크게 안 다르긴 함) 
 % 일반 MISO 설정을 어떻게 할지 생각해보기 
-
+N_UPA = N_y * N_z;
 for p0 = 1:length(tx_power)
     fprintf('tx_power=%d\n',tx_power(p0));
     pow = 1e-3*10.^(tx_power(p0)/10); %dBm -> W
@@ -19,13 +19,14 @@ for p0 = 1:length(tx_power)
         ratec = zeros(1,batch_size);
         for b0 = 1:batch_size
             % Channel vector between IRS and user
-            h = zeros(N_IRS,1);
+            h = zeros(N_UPA,1);
             for l=1:L
                 hl = sqrt(G_BS)*path_gain*sqrt(0.5).*(randn(1,1)+1j*randn(1,1));
-                h = h + hl.* channel_ht(theta_l_epoch(l,1),phi_l_epoch(l,1),N_IRS,d,lambda);
+                h = h + hl.* channel_ht_any(theta_l_epoch(l,1),phi_l_epoch(l,1),N_y,N_z,d,lambda);
+%                 h = h + hl.* channel_ht(theta_l_epoch(l,1),phi_l_epoch(l,1),N_UPA,d,lambda);
             end
             h = h./sqrt(L);
-            ratec(b0) = log2(1+pow/N_IRS.*real(h'*h));
+            ratec(b0) = log2(1+pow.*real(h'*h));
         end
         rate(ite) = mean(ratec);
     end
@@ -33,4 +34,4 @@ for p0 = 1:length(tx_power)
 end
 figure
 plot(tx_power,rate_w,'ro-');
-save('MISO.mat','tx_power','rate_w','N_IRS');
+save('MISO.mat','tx_power','rate_w','N_UPA');
